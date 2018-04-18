3790bf75402a4470a124411b020741493be7f7da
Jason Glover <jasontglover@gmail.com>
2018-04-18 15:42:35 -0700

    Lecture 11; seeders, database practice

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -8,12 +8,140 @@ use App;
 use Debugbar;
 use IanLChapman\PigLatinTranslator\Parser;
 use Carbon\Carbon;
+use App\Book;
 
 class PracticeController extends Controller
 {
 
+    public function practice17()
+    # Assignment_11_Practice005 = Remove any/all books by the author “J.K. Rowling”..
+    {
+        $results = Book::where('author', '=', 'J.K. Rowling')->delete();
+        dump($results);
+    }
+
+    public function practice16()
+    # Assignment_11_Practice004 = Retrieve all the books in descending order according to published date..
+    {
+        $results = Book::orderBy('published_year', 'desc')->get();
+        dump($results->toArray());
+    }
+
+    public function practice15()
+    # Assignment_11_Practice003 = Retrieve all the books in alphabetical order by title.
+    {
+        $results = Book::orderBy('title', 'asc')->get();
+        dump($results->toArray());
+    }
+
+    public function practice14()
+    # Assignment_11_Practice002 = Retrieve all the books published after 1950.
+    {
+        $results = Book::where('published_year', '>', '1950')->get();
+        dump($results->toArray());
+    }
+
+    public function practice13()
+    # Assignment_11_Practice001 = Retrieve the last 2 books that were added to the books table.
+    {
+        $results = Book::orderBy('id', 'desc')->limit(2)->get();
+        dump($results->toArray());
+    }
+
+    public function practice12()
+    # Assignment_11_Example = Retrieve all the books authored by J.K Rowling
+    {
+        $results = Book::where('author', '=', 'J.K. Rowling')->get();
+        dump($results->toArray());
+    }
+
+    public function practice11()
+    // Delete
+    {
+        # First get a book to delete
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
+
+        if (!$book) {
+            dump('Did not delete- Book not found.');
+        } else {
+            $book->delete();
+            dump('Deletion complete; check the database to see if it worked...');
+        }
+    }
+
+    public function practice10()
+    // Update
+    {
+        # First get a book to update
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
+
+        if (!$book) {
+            dump("Book not found, can't update.");
+        } else {
+            # Change some properties
+            $book->title = 'The Really Great Gatsby';
+            $book->published_year = '2025';
+
+            # Save the changes
+            $book->save();
+
+            dump('Update complete; check the database to confirm the update worked.');
+        }
+    }
+
+    public function practice9()
+    // Read (method 2)
+    {
+        //$book = new Book();
+        $books = Book::where('title', 'LIKE', '%Harry Potter%')->get();
+
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+    public function practice8()
+    // Read
+    {
+        $book = new Book();
+        $books = $book->where('title', 'LIKE', '%Harry Potter%')->get();
+
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
 
 
+    public function practice7()
+    // Create/Add
+    {
+        # Instantiate a new Book Model object
+        $book = new Book();
+
+        # Set the properties
+        # Note how each property corresponds to a field in the table
+        // column = value
+        $book->title = 'Harry Potter and the Sorcerer\'s Stone';
+        $book->author = 'J.K. Rowling';
+        $book->published_year = 1997;
+        $book->cover_url = 'http://prodimage.images-bn.com/pimages/9780590353427_p0_v1_s484x700.jpg';
+        $book->purchase_url = 'http://www.barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-j-k-rowling/1100036321?ean=9780590353427';
+
+        # Invoke the Eloquent `save` method to generate a new row in the
+        # `books` table, with the above data
+        $book->save();
+
+        //dump('Added: '.$book->title);
+        dump($book);
+    }
 
     public function practice5()
     {
